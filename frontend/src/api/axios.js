07739257e637f5a442f4ec4224b57bbd1ff7f62f
ShivaXXD <lucki22813@stud.op.edu.ua>
2025-12-06 21:39:09 +0200

    update axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // Адреса вашого Laravel сервера
+    baseURL: 'api', 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
